cb9d79f99569d7bde2d391311864ee9b599eab1d
zus <zakharklimovich@gmail.com>
2025-10-31 01:45:50 +0300

    initital commit

---
+++ products-filter/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Список заданий</h1>
+    <div class="products">
+      <TaskItem 
+        v-for="task in tasks" 
+        :key="task._id"  
+        :task="task"
+       >
+      </TaskItem>
+    </div>
+
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import TaskItem from "./components/TaskItem.vue";
+
+const tasks = [
+  { _id: "1", title: "Завтрак", text: "Приготовить завтрак и поесть" },
+  { _id: "2", title: "Прогулка", text: "После пар пройтись погулять" },
+  { _id: "3", title: "Сделать дз", text: "Вечером выполнить дз" },
+  { _id: "4", title: "Ужин", text: "Приготовить ужин и поесть" },
+];
+
+</script>
+
+<style>
+.products {
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+}
+</style>
---
+++ products-filter/src/components/TaskItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="task-card">
+    <header>
+      <div><b>{{ props.task.title }}</b></div>
+    </header>
+
+    <main>
+      <div> Задание: {{ props.task.text }}. </div>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <button type="button" @click="complete">Выполнить</button>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  task: { title: string; text: string };
+}>();
+
+function complete() {
+  alert(`Вы выполнили задание ${props.task.title}`);
+}
+</script>
+
+<style>
+.task-card {
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  border: 1px solid #ccc;
+  padding: 12px;
+  border-radius: 8px;
+}
+</style>
